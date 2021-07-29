3882abd6e06d0e2d4e10a0b61b5661425dbc552d
Ketan <ketantoo@gmail.com>
2021-07-30 00:18:04 +0530

    findDuplicate

---
+++ day 1/findDuplicate.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/submissions/
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let obj = {}
+    for(let num of nums){
+        if(obj[num]){
+            return num;
+        }else{
+            obj[num] = 1;
+        }
+    }
+    
+};
